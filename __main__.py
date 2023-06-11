8eb7a38d46338c05ee33e5ef017c25b63f7ae8be
outragedline <outragedline@proton.me>
2023-06-11 02:53:17 -0300

    Its almost working

--- __main__.py
+++ __main__.py
@@ -4,16 +4,26 @@ import os
 import sys
 import math
 
-def separate(entrys):
+def separate(entrys, path, c=0):
+    c+=1
     num_entrys = len(entrys)
     sliced_entrys = []
     if num_entrys == 0:
         return
     
     while len(sliced_entrys) <= 30 and len(entrys) > 0:
-        sliced_entrys.append(entrys.pop())
+        sliced_entrys.append(entrys.pop(0))
 
-    separate(entrys)
+    sub_dir_path = os.path.join(path, f"package{c}")
+    os.mkdir(sub_dir_path)
+
+    for entry in sliced_entrys:
+        entry_path = os.path.join(path, entry)
+        new_entry_path = os.path.join(sub_dir_path, entry)
+        os.rename(entry_path, new_entry_path)
+    separate(entrys,c)
+    
+    
         
 def organize(path):
     entrys = []
@@ -23,7 +33,7 @@ def organize(path):
             organize(entry_path)
         else:
             entrys.append(entry.name)
-    separate(entrys)
+    separate(entrys, path)
             
         
 
